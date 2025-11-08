424f07a926d8ed576a5e8d34f65ffccc6e329f73
MS-KyuZ0 <muhammadsadam729@gmail.com>
2025-11-08 09:05:46 +0800

    Refactor findBestAnswer function to utilize aliases for improved response matching

--- src/Utils/knowledgeBase.ts
+++ src/Utils/knowledgeBase.ts
@@ -1,17 +1,21 @@
 import * as Config from '../../config.json'
 
 export function findBestAnswer(input: string): string | null {
-    const knowledgeBase: Record<string, string> = Config.knowledgeBase
+    const knowledgeBase = Config.knowledgeBase as Record<string, {aliases: string[], answer: string}>
     const inputLower = input.toLowerCase().normalize('NFKC')
         .replace(/[^\w\s]/gi, ' ')
         .replace(/\s+/g, ' ')
         .trim()
 
     for (const key in knowledgeBase) {
-        const keyLower = key.toLowerCase().normalize('NFKC').replace(/\s+/g, ' ').trim()
-        const regex = new RegExp(`\\b${keyLower}\\b`)
-        if (regex.test(inputLower)) {
-            return knowledgeBase[key]
+        const {aliases, answer} = knowledgeBase[key]
+
+        for (const alias of aliases){
+            const isText = alias.toLocaleLowerCase().normalize("NFKC").trim()
+            const regex = new RegExp(`\\b${isText}\\b`)
+            if (regex.test(inputLower)){
+                return answer
+            }
         }
     }
 
